7343ec715dd061b794a4ec8b588ab4655e8e3184
Brian Starkey <stark3y@gmail.com>
2020-04-27 22:36:39 +0100

    WIP: Power up and read GPS

--- main/main.c
+++ main/main.c
@@ -7,37 +7,113 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <stdbool.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_system.h"
+#include "esp_sleep.h"
 #include "esp_spi_flash.h"
+
+#include "i2c_helper.h"
+#include "axp192.h"
+
+
+#define GPS_UART_TXD (GPIO_NUM_12)
+#define GPS_UART_RXD (GPIO_NUM_34)
+
+#define LORA_SCK     (GPIO_NUM_5)
+#define LORA_MISO    (GPIO_NUM_19)
+#define LORA_MOSI    (GPIO_NUM_27)
+#define LORA_SS      (GPIO_NUM_18)
+#define LORA_DI0     (GPIO_NUM_26)
+#define LORA_RST     (GPIO_NUM_23)
+
+#define AXP192_I2C_SDA (GPIO_NUM_21)
+#define AXP192_I2C_SCL (GPIO_NUM_22)
+#define AXP192_IRQ     (GPIO_NUM_35)
+
+#define USER_BTN       (GPIO_NUM_36)
+
+#define HEADER_VOLTAGE_RAIL AXP192_RAIL_DCDC1
+#define ESP32_VOLTAGE_RAIL  AXP192_RAIL_DCDC3
+#define GPS_VOLTAGE_RAIL    AXP192_RAIL_LDO3
+#define LORA_VOLTAGE_RAIL   AXP192_RAIL_LDO2
+
+/*
+ * GPS is on:  LDO3 (3v3)
+ * LoRa is on: LDO2 (3v3)
+ * R55 is pop
+ * DCDC1 is "VCC_2.5V" - only goes to headers
+ * DCDC2 is NC
+ * DCDC3 is main +3v3 (ESP ++)
+ * VSYS is 5V
+ * VBUS0 goes to LDO, only for CP2104 (ME6211)
+ * LDO1 is VCC_RTC - connected to GPS backup battery
+ * LDOIO is NC
+ *
+ * SysEN is connected to LDO1
+ * N_OE is floating
+ */
+
+#define BUF_SIZE (1024)
 
 void app_main(void)
 {
     printf("Hello world!\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU cores, WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    i2c_init();
 
-    printf("silicon revision %d, ", chip_info.revision);
+    axp192_t axp = {
+        .read = &i2c_read,
+        .write = &i2c_write,
+    };
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    axp192_init(&axp);
 
-    printf("Free heap: %d\n", esp_get_free_heap_size());
+    // Power off everything we don't need
+    axp192_set_rail_state(&axp, AXP192_RAIL_DCDC1, false);
+    axp192_set_rail_state(&axp, AXP192_RAIL_DCDC2, false);
+    axp192_set_rail_state(&axp, AXP192_RAIL_LDO2, false);
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // Set the GPS voltage and power it up
+    axp192_set_rail_state(&axp, AXP192_RAIL_LDO3, false);
+    axp192_set_rail_millivolts(&axp, AXP192_RAIL_LDO3, 3300);
+    axp192_set_rail_state(&axp, AXP192_RAIL_LDO3, true);
+
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    uart_driver_install(UART_NUM_1, BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, GPS_UART_TXD, GPS_UART_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    int i;
+    for (i = 0; i < 3000; i++) {
+        // Read data from the UART
+        int len = uart_read_bytes(UART_NUM_1, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+        // Write data back to the UART
+        //uart_write_bytes(UART_NUM_1, (const char *) data, len);
+	fwrite(data, 1, len, stdout);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        fflush(stdout);
     }
-    printf("Restarting now.\n");
+
+    printf("Sleeping...\n");
     fflush(stdout);
-    esp_restart();
+    axp192_set_rail_state(&axp, AXP192_RAIL_LDO3, false);
+    esp_deep_sleep(5000000);
 }
